6e57c2b8f50dbd3c74d38edc236c1f27b2e01e4a
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2022-11-08 10:24:04 +0800

    write the list of all of the (fucking) cities in the Philippines
    SELECT CITY
    Alaminos
    Angeles City
    Antipolo
    Bacolod
    Bacoor
    Bago
    Baguio
    Bais
    Balanga
    Batac
    Batangas City
    Bayawan
    Baybay
    Bayugan
    Biñan
    Bislig
    Bogo
    Borongan
    Butuaan
    Cadbadbaran
    Cabanatuan
    Cabuyao
    Cadiz
    Cagayan de Oro
    Calaca
    Calamba
    Calapan
    Calbayog
    Caloocan
    Candon
    Canlaon
    Carcar
    Catbalogan
    Cauayan
    Cavite City
    Ceby City
    Cotabato City
    Dagupan
    Danao
    Dapitan
    Dasmariñas
    Davao City
    Digos
    Dipolog
    Dumaguete
    El Salvador
    Escalante
    Gapan
    General Santos
    General Trias
    Gingoog
    Guihulngan
    Himamaylan
    Ilagan
    Iligan
    Iloilo City
    Imus
    Iriga
    Isabela
    Kabankalan
    Kidapawan
    Koronadal
    La Carlota
    Lamitan
    Laoag
    Lapu-Lapu City
    Las Piñas
    Legazpi
    Ligao
    Lipa
    Lucena
    Maasin
    Mabalacat
    Makati
    Malabon
    Malaybalay
    Malolos
    Mandaluyong
    Mandaue
    Manila
    Marawi
    Marikina
    Masbate City
    Mati
    Mecauayan
    Muñoz
    Muntinlupa
    Naga (Camarines Sur)
    Naga (Cebu)
    Navotas
    Olongapo
    Ormoc
    Oroquieta
    Ozamiz
    Pagadian
    Palayan
    Panabo
    Parañaque
    Pasay
    Pasig
    Passi
    Puerto Princessa
    Quezon City
    Roxas
    Sagay
    Samal
    San Carlos (Negros Occidental)
    San Carlos (Pangasinan)
    San Fernando (La Union)
    San Fernando (Pampanga)
    San Jose
    San Jose del Monte
    San Juan
    San Pablo
    San Pedro
    Santa Rosa
    Santo Tomas
    Santiago
    Silay
    Sipalay
    Sorsogon City
    Surigao City
    Tabaco
    Tabuk
    Tacloban
    Tacurong
    Tagaytay
    Tagbilaran
    Taguig
    Tagum
    Talisay (Cebu)
    Talisay (Negros Occidental)
    Tanauan
    Tandag
    Tangub
    Tanjay
    Tarlac City
    Tayabas
    Toledo
    Trece Martires
    Tuguegarao
    Urdaneta
    Valencia
    Valenzuela
    Victoras
    Vigan
    Zamboanga City

--- FloodLert/src/main/java/com/mycompany/floodlert/Dashboard.java
+++ FloodLert/src/main/java/com/mycompany/floodlert/Dashboard.java
@@ -27,20 +27,29 @@ public class Dashboard extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jComboBox1 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "SELECT CITY", "Alaminos", "Angeles City", "Antipolo", "Bacolod", "Bacoor", "Bago", "Baguio", "Bais", "Balanga", "Batac", "Batangas City", "Bayawan", "Baybay", "Bayugan", "Biñan", "Bislig", "Bogo", "Borongan", "Butuaan", "Cadbadbaran", "Cabanatuan", "Cabuyao", "Cadiz", "Cagayan de Oro", "Calaca", "Calamba", "Calapan", "Calbayog", "Caloocan", "Candon", "Canlaon", "Carcar", "Catbalogan", "Cauayan", "Cavite City", "Ceby City", "Cotabato City", "Dagupan", "Danao", "Dapitan", "Dasmariñas", "Davao City", "Digos", "Dipolog", "Dumaguete", "El Salvador", "Escalante", "Gapan", "General Santos", "General Trias", "Gingoog", "Guihulngan", "Himamaylan", "Ilagan", "Iligan", "Iloilo City", "Imus", "Iriga", "Isabela", "Kabankalan", "Kidapawan", "Koronadal", "La Carlota", "Lamitan", "Laoag", "Lapu-Lapu City", "Las Piñas", "Legazpi", "Ligao", "Lipa", "Lucena", "Maasin", "Mabalacat", "Makati", "Malabon", "Malaybalay", "Malolos", "Mandaluyong", "Mandaue", "Manila", "Marawi", "Marikina", "Masbate City", "Mati", "Mecauayan", "Muñoz", "Muntinlupa", "Naga (Camarines Sur)", "Naga (Cebu)", "Navotas", "Olongapo", "Ormoc", "Oroquieta", "Ozamiz", "Pagadian", "Palayan", "Panabo", "Parañaque", "Pasay", "Pasig", "Passi", "Puerto Princessa", "Quezon City", "Roxas", "Sagay", "Samal", "San Carlos (Negros Occidental)", "San Carlos (Pangasinan)", "San Fernando (La Union)", "San Fernando (Pampanga)", "San Jose", "San Jose del Monte", "San Juan", "San Pablo", "San Pedro", "Santa Rosa", "Santo Tomas", "Santiago", "Silay", "Sipalay", "Sorsogon City", "Surigao City", "Tabaco", "Tabuk", "Tacloban", "Tacurong", "Tagaytay", "Tagbilaran", "Taguig", "Tagum", "Talisay (Cebu)", "Talisay (Negros Occidental)", "Tanauan", "Tandag", "Tangub", "Tanjay", "Tarlac City", "Tayabas", "Toledo", "Trece Martires", "Tuguegarao", "Urdaneta", "Valencia", "Valenzuela", "Victoras", "Vigan", "Zamboanga City" }));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 228, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jComboBox1, 0, 216, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 213, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(185, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -99,6 +108,7 @@ public class Dashboard extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
