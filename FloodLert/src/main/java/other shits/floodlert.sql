f88122e4598624d591950d90b3c62a3993d5c97f
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2022-11-16 14:33:39 +0800

    upload new Floodlert database

--- FloodLert/src/main/java/other shits/floodlert.sql
+++ FloodLert/src/main/java/other shits/floodlert.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2022 at 02:15 AM
+-- Generation Time: Nov 16, 2022 at 07:32 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `floodreport`
+--
+
+CREATE TABLE `floodreport` (
+  `city` varchar(60) NOT NULL,
+  `floodLevel` int(1) NOT NULL,
+  `reporterUsername` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `floodreport`
+--
+
+INSERT INTO `floodreport` (`city`, `floodLevel`, `reporterUsername`) VALUES
+('Makati', 2, 'Mozes'),
+('Makati', 2, 'Mozes'),
+('Makati', 3, 'Mozes'),
+('Makati', 1, 'Mozes'),
+('Alaminos', 3, 'hi');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `login`
 --
 
@@ -31,6 +54,15 @@ CREATE TABLE `login` (
   `username` varchar(60) NOT NULL,
   `password` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`) VALUES
+('hi', 'GODRIZZ'),
+('Mozes', 'Password'),
+('penis', 'vag');
 
 --
 -- Indexes for dumped tables
