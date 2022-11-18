d0f5f0bfbe4493f86a275c4a06b45c87d10a9ec9
mozes-lol <johnmarkgarcia164@gmail.com>
2022-11-18 13:42:16 +0800

    switch look and feel to "Windows"

--- FloodLert/src/main/java/com/mycompany/floodlert/Dashboard.java
+++ FloodLert/src/main/java/com/mycompany/floodlert/Dashboard.java
@@ -293,7 +293,7 @@ public class Dashboard extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
